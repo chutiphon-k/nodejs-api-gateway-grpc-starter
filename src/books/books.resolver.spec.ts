4ae6ae3ead85a5b2e0255e878965a9c04f0efaf2
chutiphonk <chutiphon.ksp@gmail.com>
2019-07-21 15:26:57 +0700

    commit

--- src/books/books.resolver.spec.ts
+++ src/books/books.resolver.spec.ts
@@ -6,6 +6,9 @@ import { BooksResolver } from './books.resolver';
 import { BooksService } from './books.service';
 import { BooksLoader } from '../loaders/loaders';
 
+jest.mock('@nestjs/passport/dist/auth.guard', jest.fn().mockImplementation(() => ({
+  AuthGuard: jest.fn().mockReturnValue(jest.fn()),
+})));
 jest.mock('./books.service');
 jest.mock('../loaders/loaders');
 
@@ -15,7 +18,7 @@ describe('BooksResolver', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+      imports: [PassportModule],
       providers: [
         BooksResolver,
         BooksService,
